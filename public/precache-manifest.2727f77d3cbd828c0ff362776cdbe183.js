d02c28543c07c0ab4336673a5ee7435a1f531be3
Curtis Larsen <curtis.larssen@gmail.com>
2020-09-23 20:33:20 +1200

    Created Static site

---
+++ public/precache-manifest.2727f77d3cbd828c0ff362776cdbe183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33070e5aa65406901ea99ea184b77b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1525ef04a2182de682d2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3440eb8d97d68c7b7042",
+    "url": "/static/js/2.39ed314f.chunk.js"
+  },
+  {
+    "revision": "fc519b2480babfb9b3cbdc6be3d6f1fc",
+    "url": "/static/js/2.39ed314f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1525ef04a2182de682d2",
+    "url": "/static/js/main.36cf4f71.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
